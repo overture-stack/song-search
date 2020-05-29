d1901105c425d910d1223caf792f2c3955db0c3a
Alex <lepsalex@gmail.com>
2020-05-29 14:10:37 -0400

    start.spring.io

---
+++ src/main/java/bio/overture/songsearch/SongSearchApplication.java
@@ -0,0 +1,13 @@
+package bio.overture.songsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongSearchApplication.class, args);
+	}
+
+}

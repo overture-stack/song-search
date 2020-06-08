8f75e324867bd43a1b91d48e0e53c6072964f088
Alex <lepsalex@gmail.com>
2020-06-08 14:52:54 -0400

    search fields as config class

---
+++ src/main/java/bio/overture/songsearch/config/SearchFields.java
@@ -0,0 +1,13 @@
+package bio.overture.songsearch.config;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class SearchFields {
+  public static final String ANALYSIS_ID = "analysisId";
+  public static final String ANALYSIS_TYPE = "analysisType";
+  public static final String ANALYSIS_VERSION = "analysisVersion";
+  public static final String ANALYSIS_STATE = "analysisState";
+}

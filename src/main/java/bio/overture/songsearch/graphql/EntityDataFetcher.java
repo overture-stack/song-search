8935ca5732fa4b4a356d27af99fb84ba918f83f1
Alex <lepsalex@gmail.com>
2020-06-16 11:03:20 -0400

    labeling change and setting up for the future

--- src/main/java/bio/overture/songsearch/graphql/EntityDataFetcher.java
+++ src/main/java/bio/overture/songsearch/graphql/EntityDataFetcher.java
@@ -17,44 +17,47 @@ import static java.util.stream.Collectors.toList;
 @Slf4j
 @Component
 public class EntityDataFetcher {
-    public static final String ANALYSIS_ENTITY = "Analysis";
-    public static final String FILE_ENTITY = "File";
-    public static final String RUN_ENTITY = "Run";
+  public static final String ANALYSIS_ENTITY = "Analysis";
+  public static final String FILE_ENTITY = "File";
+  public static final String RUN_ENTITY = "Run";
 
-    private final AnalysisService analysisService;
-    private final FileService fileService;
+  private final AnalysisService analysisService;
+  private final FileService fileService;
 
-    @Autowired
-    public EntityDataFetcher(AnalysisService analysisService, FileService fileService) {
-        this.analysisService = analysisService;
-        this.fileService = fileService;
-    }
+  @Autowired
+  public EntityDataFetcher(AnalysisService analysisService, FileService fileService) {
+    this.analysisService = analysisService;
+    this.fileService = fileService;
+  }
 
-    public DataFetcher getDataFetcher() {
-        return environment ->
-            environment.<List<Map<String, Object>>>getArgument(_Entity.argumentName).stream()
-                .map(
-                    values -> {
-                        if (ANALYSIS_ENTITY.equals(values.get("__typename"))) {
-                            final Object analysisId = values.get("analysisId");
-                            if (analysisId instanceof String) {
-                                return analysisService.getAnalysisById((String) analysisId);
-                            }
-                        }
-                        if (FILE_ENTITY.equals(values.get("__typename"))) {
-                            final Object fileObjectId = values.get("objectId");
-                            if (fileObjectId instanceof String) {
-                                return fileService.getFileByObjectId((String) fileObjectId);
-                            }
-                        }
-                        if (RUN_ENTITY.equals(values.get("__typename"))) {
-                            final Object runName = values.get("runName");
-                            if (runName instanceof String) {
-                                return new Run((String) runName, analysisService.getAnalysesByRunName((String) runName));
-                            }
-                        }
-                        return null;
-                    })
-                .collect(toList());
-    }
+  public DataFetcher getDataFetcher() {
+    return environment ->
+        environment.<List<Map<String, Object>>>getArgument(_Entity.argumentName).stream()
+            .map(
+                values -> {
+                  if (ANALYSIS_ENTITY.equals(values.get("__typename"))) {
+                    final Object analysisId = values.get("analysisId");
+                    if (analysisId instanceof String) {
+                      return analysisService.getAnalysisById((String) analysisId);
+                    }
+                  }
+                  if (FILE_ENTITY.equals(values.get("__typename"))) {
+                    final Object fileObjectId = values.get("objectId");
+                    if (fileObjectId instanceof String) {
+                      return fileService.getFileByObjectId((String) fileObjectId);
+                    }
+                  }
+                  if (RUN_ENTITY.equals(values.get("__typename"))) {
+                    final Object runName = values.get("runName");
+                    if (runName instanceof String) {
+                      return new Run(
+                          (String) runName,
+                          analysisService.getAnalysesByRunName((String) runName),
+                          "Not Implemented!");
+                    }
+                  }
+                  return null;
+                })
+            .collect(toList());
+  }
 }

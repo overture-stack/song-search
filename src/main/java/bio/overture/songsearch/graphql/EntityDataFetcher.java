040b84cf1aff8d1cfe2c60c4e2ce6963cb1edc6c
jaserud <udjaser@gmail.com>
2020-07-02 16:26:22 -0400

    implement inputAnalyses in DataFetcher for Runs

--- src/main/java/bio/overture/songsearch/graphql/EntityDataFetcher.java
+++ src/main/java/bio/overture/songsearch/graphql/EntityDataFetcher.java
@@ -4,8 +4,11 @@ import bio.overture.songsearch.model.Run;
 import bio.overture.songsearch.service.AnalysisService;
 import bio.overture.songsearch.service.FileService;
 import com.apollographql.federation.graphqljava._Entity;
+import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableMap;
 import graphql.schema.DataFetcher;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -49,15 +52,39 @@ public class EntityDataFetcher {
                   }
                   if (RUN_ENTITY.equals(values.get("__typename"))) {
                     final Object runId = values.get("runId");
+                    val filters = createAnalysisFiltersFromRunParameters(values.get("parameters"));
+
                     if (runId instanceof String) {
                       return new Run(
                           (String) runId,
                           analysisService.getAnalysesByRunId((String) runId),
-                          "Not Implemented!");
+                          analysisService.getAnalyses(filters, null)
+                          );
                     }
                   }
                   return null;
                 })
             .collect(toList());
+  }
+
+  @SuppressWarnings("unchecked")
+  private ImmutableMap<String, Object> createAnalysisFiltersFromRunParameters(Object parametersObj) {
+      // conver parameters to map
+      val parametersBuilder = ImmutableMap.<String, Object>builder();
+      if (parametersObj != null) {
+          parametersBuilder.putAll((Map<String, Object>) parametersObj);
+      }
+      val parameters = parametersBuilder.build();
+
+      List<String> lookFor = List.of("analysis_id", "normal_aln_analysis_id", "tumour_aln_analysis_id"); // TODO put in more common place?
+
+      val filtersBuilder = ImmutableMap.<String, Object>builder();
+      lookFor.forEach(k -> {
+          if (parameters.get(k) != null) {
+              val formattedKey = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, k.toLowerCase());  // TODO check if this is needed
+              filtersBuilder.put(formattedKey, parameters.get(k));
+          }
+      });
+      return filtersBuilder.build();
   }
 }
